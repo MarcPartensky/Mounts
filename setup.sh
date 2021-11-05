e6851db9515d4d9938335db9d41e665270099c42
marc <marc.partensky@gmail.com>
2021-11-05 19:18:41 +0100

    bin/ls

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 ln -sf *.mount /lib/systemd/system
 systemctl daemon-reload
-for mountfile in $(ls *.mount); do
+for mountfile in $(/bin/ls *.mount); do
 	systemctl start /lib/systemd/system/$mountfile
 	systemctl enable /lib/systemd/system/$mountfile
 done
