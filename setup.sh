9e16cef67fc9eb33b4459e82ebf7b0a98b5fb2ca
marc <marc.partensky@gmail.com>
2021-11-05 19:20:37 +0100

    restart

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 ln -sf $PWD/*.mount /lib/systemd/system
 systemctl daemon-reload
 for mountfile in $(/bin/ls *.mount); do
-	systemctl start /lib/systemd/system/$mountfile
+	systemctl restart /lib/systemd/system/$mountfile
 	systemctl enable /lib/systemd/system/$mountfile
 done
